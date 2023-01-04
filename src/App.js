018f259ec8d1b782a6fdd73bdfee5d28f264602e
Olha1304 <olhakolisnyk1@icloud.com>
2023-01-04 20:06:44 +0200

    Add space

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
       <footer>
         This project created by Olha Kolisnyk{" "}
         <a href="https://github.com/Olha1304/react-5.git">
-          open-sourced in GitHub
+          open-sourced in GitHub{" "}
         </a>
         <a href="https://dynamic-banoffee-75a1c5.netlify.app">
           hosted by Netlify
